0ed61a848d3e41bf6e39fab29963cf8ea6d94bfa
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-07-06 16:44:40 -0300

    feat(layout): add layout structure with header and providers
    integrate `Header` and `Providers` components into root layout. Refactor layout to include basic styling and component structure. Add `next-auth` for session management. Update dependencies in `package.json` and `pnpm-lock.yaml`.

---
+++ app/Header.tsx
@@ -0,0 +1,6 @@
+export default function Header() {
+    return <>
+        <h1 className="text-4xl font-bold">Nexus</h1>
+        <p className="text-xl">A Next.js + Prisma + TypeScript starter</p>
+    </>;
+}
